bb965beffeab39647506d24bd17e894ae3ce8416
IvanStrelka <i.strelka@blanc.ru>
2023-11-29 16:24:10 +0300

    add

--- src/main/kotlin/kotlinCore/alg/Second.kt
+++ src/main/kotlin/kotlinCore/alg/Second.kt
@@ -1,12 +1,14 @@
 package kotlinCore.alg
 
-import org.junit.Assert.assertArrayEquals
+import java.util.*
 
 
 fun main() {
-    assertArrayEquals(intArrayOf(2, 4, 6), maps(intArrayOf(1, 2, 3)))
-    assertArrayEquals(intArrayOf(8, 2, 2, 2, 8), maps(intArrayOf(4, 1, 1, 1, 4)))
-    assertArrayEquals(intArrayOf(4, 4, 4, 4, 4, 4), maps(intArrayOf(2, 2, 2, 2, 2, 2)))
+//    assertArrayEquals(intArrayOf(2, 4, 6), maps(intArrayOf(1, 2, 3)))
+//    assertArrayEquals(intArrayOf(8, 2, 2, 2, 8), maps(intArrayOf(4, 1, 1, 1, 4)))
+//    assertArrayEquals(intArrayOf(4, 4, 4, 4, 4, 4), maps(intArrayOf(2, 2, 2, 2, 2, 2)))
+    println(isPalindrome2("ollo"))
+
 }
 
 fun evenOrOdd(number: Int): String {
@@ -57,3 +59,27 @@ fun maps(x: IntArray): IntArray {
 }
 
 fun maps2(x: IntArray): IntArray = IntArray(x.size, { x[it] * 2 })
+
+
+fun isPalindrome(text: String): Boolean {
+    // Удаляем все не-буквенно-цифровые символы и приводим к нижнему регистру
+    val cleanedText = text.replace("[^a-zA-Z0-9]".toRegex(), "").lowercase(Locale.getDefault())
+
+    // Проверяем, является ли очищенный текст палиндромом
+    return cleanedText == StringBuilder(cleanedText).reverse().toString()
+}
+
+fun isPalindrome2(text: String): Boolean {
+    val cleanedText = text.replace("[^a-zA-Z0-9]".toRegex(), "").lowercase(Locale.getDefault())
+    var forward = 0
+    var backward = cleanedText.length - 1
+
+    while (backward > forward) {
+        if (cleanedText.get(forward) != cleanedText.get(backward)) {
+            return false
+        }
+        forward++
+        backward--
+    }
+    return true
+}
